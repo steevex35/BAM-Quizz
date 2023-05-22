7300ea128d2f141a03e9be0f0053653913e26d35
Steeves Obiang Ndam <steevesobiang@gmail.com>
2023-05-23 01:49:30 +0200

    clean up and final design

--- src/components/ranking/ranking.ts
+++ src/components/ranking/ranking.ts
@@ -4,6 +4,7 @@ import { I18N } from 'aurelia-i18n';
 import { Router } from 'aurelia-router';
 import internal from 'stream';
 import { User } from 'user';
+import { bindable } from 'aurelia-framework';
 @autoinject
 
 
@@ -12,12 +13,20 @@ export class ranking{
 
   private currentUser: User;
   private listOfGamers = [];
+  private intervalId;
 
   constructor(private router:Router, private i18n:I18N){
     this.i18n.setLocale('fr');
-     this.fetchGamer()
-    setInterval(this.fetchGamer,30000);
-   
+    this.fetchGamer()
+    this.intervalId= setInterval(() => {
+      // window.location.reload();
+      this.fetchGamer()
+       }, 30000);
+  }
+
+
+  attached() {
+  
   }
 
   async fetchGamer(){
@@ -31,7 +40,7 @@ export class ranking{
     .then(response => response.json())
     .then(async data => {
        this.listOfGamers=data
-       console.log("we get datas : "+data)
+       //console.log("we get datas : "+data)
       })
       .catch(error => {
         console.log(error)
